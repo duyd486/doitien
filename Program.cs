959ca4895fba81d3d6684f893f4d7a0c724c2f27
duyd486 <duyd486@gmail.com>
2024-05-04 01:50:53 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap so $ can doi: ");
+        float dola = float.Parse(Console.ReadLine());
+        float vnd = dola * 23000;
+        Console.WriteLine("So tien khi chuyen qua vnd la: " + vnd);
+    }
+}
